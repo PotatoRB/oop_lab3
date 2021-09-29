4ceee3701267e22552b8e19ae5997a246b84b967
PotatoRB <andrei_sml@bk.ru>
2021-09-29 21:54:50 +0300

    Добавьте файлы проекта.

---
+++ oop_lab3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_lab3
+{
+    class Airline
+    {
+        public string Destination;
+        public string Number;
+        public string Type;
+        public string Time;
+        public string Days;
+
+        public Airline()
+        {
+            Destination = "Москва";
+            Number = "SU 1234";
+            Type = "Boeing-777";
+            Time = "06.30";
+            Days = "Среда";
+        }
+        public void Info()
+        {
+            Console.WriteLine($"{Number}, {Time}, {Days}, {Destination}, {Type}");
+            Console.ReadKey();
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Airline q = new Airline();
+            q.Info();
+        }
+    }
+}
